041bc4cf9c3208e67f3590ba925a4aab6e16b16c
Almir Lima <106777254+AlmirLima150@users.noreply.github.com>
2023-04-04 17:04:24 -0300

    Add files via upload
    Novos exercícios

---
+++ exercicio_3.py
@@ -0,0 +1,14 @@
+# for x in range(0, 2500, 100):
+#     print((x))
+# for x in range(10,0,-1):
+#     print((x))
+# num=int(input("digite um número: "))
+# for x in range(1,11):
+#     tabuada = x*num
+#     print(f"{x} x {num} = {tabuada}")
+nome=input("digite um nome: ")
+i=0
+for x in nome:
+    i+=1
+print(f"O nome tem {i} letras")
+print(len(nome))
---
+++ exercicio_4.py
@@ -0,0 +1,4 @@
+num=int(input("digite um número: "))
+for impar in range(1,num):
+  if impar%2!=0:
+    print(impar)
